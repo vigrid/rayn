66d94ed0ca63098738842f0ea8739666a49fbec5
vigrid <vigrid@gamination.com>
2019-01-10 18:18:42 +0100

    Window Initialization & Shutdown

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+extern crate minifb;
+
+use minifb::{ Window, WindowOptions, Key };
+
+const WIDTH: usize = 960;
+const HEIGHT: usize = 540;
+
+const COLOR_MAGENTA: u32 = 0x00ff00ff;
+
+fn clear(buffer: &mut Vec<u32>, color: u32) {
+    for pixel in buffer.iter_mut() {
+        *pixel = color;
+    }
+}
+
+fn main() {
+    let window_options = WindowOptions::default();
+    let mut window = Window::new("RayN", WIDTH, HEIGHT, window_options).unwrap();
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        clear(&mut buffer, COLOR_MAGENTA);
+        window.update_with_buffer(&buffer).unwrap();
+    }
+}
