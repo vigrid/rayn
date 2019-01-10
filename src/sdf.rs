a90b1f5f9ceb4043ab6ed1f10410c41af6ad1079
vigrid <vigrid@gamination.com>
2019-01-10 22:47:41 +0100

    Implemented SDF Normal Estimation

--- src/sdf.rs
+++ src/sdf.rs
@@ -5,3 +5,17 @@ use cgmath::{ Vector3, InnerSpace };
 pub fn sphere(p: Vector3<f32>, r: f32) -> f32 {
     p.magnitude() - r
 }
+
+#[allow(dead_code)]
+pub fn estimate_normal(scene: fn(Vector3<f32>) -> f32, position: Vector3<f32>, epsilon: f32) -> Vector3<f32> {
+    let x1 = scene(Vector3 { x: position.x + epsilon, ..position });
+    let x0 = scene(Vector3 { x: position.x - epsilon, ..position });
+    let y1 = scene(Vector3 { x: position.y + epsilon, ..position });
+    let y0 = scene(Vector3 { x: position.y - epsilon, ..position });
+    let z1 = scene(Vector3 { x: position.z + epsilon, ..position });
+    let z0 = scene(Vector3 { x: position.z - epsilon, ..position });
+
+    let n = Vector3 { x: x1 - x0, y: y1 - y0, z: z1 - z0 };
+
+    n.normalize()
+}
